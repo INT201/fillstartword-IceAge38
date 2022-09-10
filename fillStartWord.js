233208f2ab49d6b72218d8063949a78d409dd06b
Nanthawan <88131041+IceAge38@users.noreply.github.com>
2022-09-10 18:13:45 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,19 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  
+
+   if (word == null || word == undefined){
+    return "undefined"
+  }  if (startWord === word.substring(0,startWord.length)){
+    return word
+  } else{
+  return startWord+word}
 }
+// console.log(fillStartWord("1-2565-","Hello World"))
+// console.log(fillStartWord("JS", "beginner"))
+// console.log(fillStartWord("first", "firststep"))
+// console.log(fillStartWord("first", null))
+// console.log(fillStartWord("first", undefined))
+
 module.exports = fillStartWord
